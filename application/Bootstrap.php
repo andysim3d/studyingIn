468a08c0e5d4e1371920d0450c76fb94ecc5abd7
Zhengwei Li <lzw900420@gmail.com>
2015-04-23 15:52:46 -0400

    initialize Mail and View

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,34 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+	protected function _initMail() {
 
+		try {
+			$config = array(
+				'auth' => 'login',
+				'username' => 'info.studyingin@gmail.com',
+				'password' => 'wocao,nixiangganshenme',
+				'ssl' => 'tls',
+				'port' => 587,
+			);
+
+			$mailTransport = new Zend_Mail_Transport_Smtp('smtp.gmail.com', $config);
+			Zend_Mail::setDefaultTransport($mailTransport);
+		} catch (Exception $exc) {
+			echo $exc->getMessage();
+			echo "error"; //Do something with exception
+		}
+	}
+
+	protected function _initView() {
+
+		$view = new Zend_View();
+		$view->doctype("HTML5");
+		$view->headTitle("StudyingIn");
+		$view->headLink(array('rel' => 'shortcut icon', 'href' => '/image/logo.ico'), 'PREPEND');
+		$view->headMeta()->appendHttpEquiv('Content-Type', 'text/html; charset=UTF-8');
+
+		return $view;
+	}
 
 }
-
