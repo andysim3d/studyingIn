5b2fce4502297036f611d71a5b6be1bb62824b50
Zhengwei Li <lzw900420@gmail.com>
2015-04-24 14:26:35 -0400

    add public_path to index.php and update the usercontroller and login ui

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -2,7 +2,9 @@
 
 header("Content-type: text/html;charset=utf-8");
 require_once APPLICATION_PATH . '/utils/UUID.php';
-require_once APPLICATION_PATH . '/medels/Dao/UserDao.php';
+require_once APPLICATION_PATH . '/utils/Email_Sender.php';
+require_once APPLICATION_PATH . '/models/Dao/User_Dao.php';
+require_once APPLICATION_PATH . '/models/Dao/User_Info_Dao.php';
 class UserController extends Zend_Controller_Action {
 
 	public function init() {
@@ -18,10 +20,85 @@ class UserController extends Zend_Controller_Action {
 	 *
 	 */
 	public function registerAction() {
-
 		$formRegister = new studyingIn_Form_User();
 		$formRegister->removeElement('salt');
 		$formRegister->removeElement('user_uuid');
+		$formRegister->removeElement('user_actived');
+		//$formRegister->removeElement('user_remember');
+		$this->view->formRegister = $formRegister;
+
+		if ($this->getRequest()->isPost()) {
+
+			if ($formRegister->isValid($_POST)) {
+				$userData = $formRegister->getValues();
+			} else {
+				$this->view->info = 1;
+				$this->_forward('err', 'global');
+			}
+
+			$userData['user_uuid'] = 'user-' . UUID::v4();
+			$userDao = new studyingIn_Model_User_Dao();
+			$newUser_id = $userDao->create_user($userData);
+
+			$userInfo = $userData;
+			unset($userInfo['salt']);
+			unset($userInfo['user_password']);
+			unset($userInfo['confirm_password']);
+			unset($userInfo['user_actived']);
+			$userInfo['user_id'] = $newUser_id;
+			$userInfo['user_introduction'] = "该用户很懒，暂时什么都没有留下。";
+			$userInfo['user_create_time'] = date("Y-m-d H:i:s", time());
+
+			$userInfoDao = new studyingIn_Model_User_Info_Dao();
+			$newUserInfo = $userInfoDao->create_user_info($userInfo);
+
+			if ($newUser_id && $newUserInfo) {
+				$sendActiveEmail = Email_Sender::sendActivationEmail($userData['user_name'], $userData['user_email'], $userData['salt']);
+				if ($sendActiveEmail) {
+					//TODO: jump to the index/index, and alert('we have send you a confirm email, please active your account with the url in the email ASAP.')
+					$this->view->info = 1;
+					$this->_forward('ok', 'global');
+				}
+			}
+
+		}
+
+	}
+
+	public function activeAction() {
+		// action body
+		$rcode = base64_decode($this->getRequest()->getParam('l', ''));
+		list($email, $code, $time) = explode('#', $rcode);
+
+		$userDao = new studyingIn_Model_User_Dao();
+		$where = array('user_email' => $email);
+		$user = $userDao->get_user($where);
+
+		if (!$user || $code != hash('sha1', $email . $user['salt'])) {
+			throw new Exception("Incorrect validation code!");
+		} else {
+			if (time() > ($time + 60 * 60 * 24)) {
+				//TODO:the email is over time...
+			} else {
+				$userDap->active_user_by_user_id($user['user_id']);
+				session_start();
+				$_SESSION['user_id'] = $user['user_id'];
+				$this->_redirect('user/account/id/' . $id);
+				//TODO:jump to the home page
+			}
+		}
+	}
+
+	public function loginAction() {
+
+		$formRegister = new studyingIn_Form_User();
+		$formRegister->removeElement('user_id');
+		$formRegister->removeElement('user_name');
+		$formRegister->removeElement('user_school');
+		$formRegister->removeElement('salt');
+		$formRegister->removeElement('user_uuid');
+		$formRegister->removeElement('user_actived');
+		$formRegister->removeElement('confirm_password');
 		$this->view->formRegister = $formRegister;
 
 		if ($this->getRequest()->isPost()) {
@@ -30,86 +107,13 @@ class UserController extends Zend_Controller_Action {
 				$userData = $formRegister->getValues();
 			}
 
-			$userData['user_uuid'] = UUID::v4();
-
-			$userInfo = $userData;
-			$userInfo[]
-			$userDao = new studyingIn_Model_UserDao();
-			$newUser = $user->createUser($userData);
-			if ($newUser) {
-				$this->_sendActivationEmail($userData['user_name'], $userData['user_email'], $userData['salt']);
-			}
+			$userDao = new studyingIn_Model_User_Dao();
+			$user = $userDao->get_user(array('user_email' => $userData['user_email']));
 		}
 
-	}
-
-	protected function _sendActivationEmail($name, $email, $code) {
-
-		$publicPath = PUBLIC_PATH;
-		$pathArray = explode("/", $publicPath);
-
-		$pathArrSize = count($pathArray);
-		$pathArray[$pathArrSize - 1] = 'active';
-		$newPath = implode("/", $pathArray);
-
-		$userDao = new UserDao();
-		$user = new User();
-		$user_activeTime = $userDao->updateActiveTimeByEmail($email);
-		$validationcode = 'l=' . base64_encode($email . '#' . hash('sha1', $email . $code) . '#' . $user_activeTime);
-
-		$mail = new Zend_mail('utf-8');
-		$mail->addTo($email, $name);
-		$mail->setSubject("StudyingIn注册验证");
-		$mail->setFrom('info.studyingin@gmail.com', 'StudyingIn');
-		$mail->setBodyHtml("亲爱的 <b>$name</b>,<br />" .
-			"非常感谢您注册StudyingIn.<br /><br />" .
-			"请点击链接激活您的帐号:<br />" .
-			"http://$newPath?$validationcode<br />" .
-			"如果以上链接无法点击，请将它复制到你的浏览器地址栏中进入访问，该链接24小时内有效。<br />如果此次激活请求非你本人所发，请忽略本邮件。<br /><br />再次感谢您注册StudyingIn。<br />StudyingIn 团队<br />", $charset = "utf-8");
-		return $mail->send();
-	}
-
-	public function activeAction() {
-		// action body
-		$userdao = new UserDao();
-		$rcode = base64_decode($this->getRequest()->getParam('l', ''));
-		list($email, $code, $user_activeTime) = explode('#', $rcode);
-
-		$user = new User();
-		$ok = $userdao->findUserByUser_email($email, $user);
-		$userInfo = array('user_id' => $user->getUser_id(),
-			'user_name' => $user->getUser_name(),
-			'user_email' => $user->getUser_email(),
-			'user_school' => $user->getUser_school(),
-			'user_grade' => $user->getUser_grade(),
-			'user_status' => $user->getUser_status(),
-			'whatever' => $user->getsalt());
-		if (!$ok || $code != hash('sha1', $email . $user->getSalt())) {
-			throw new Exception("Incorrect validation code!");
-		} else {
-			if (time() > ($user_activeTime + 60 * 60 * 24)) {
-				$this->view->info = 7;
-				$this->_forward('err', 'global');
-			} else {
-				$user->isActive();
-				$userdao->updateUser_statusByUser_id($user->getUser_id(), $user->getUser_status());
-				session_start();
-				$_SESSION['user'] = $userInfo;
-				$this->view->info = 6;
-				$this->_forward('ok', 'global');
-			}
-		}
-	}
-
-	public function loginAction() {
-		// action body
 	}
 
 	public function logoutAction() {
-		// action body
-	}
-
-	public function refreshcaptchaAction() {
 		// action body
 	}
 
