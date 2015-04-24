4bca69b95e0f9e724f6c5c6ec5bce0b136e9c6d4
Zhengwei Li <lzw900420@gmail.com>
2015-04-24 14:18:29 -0400

    add the GlobalController which is to control the path

---
+++ application/controllers/GlobalController.php
@@ -0,0 +1,32 @@
+<?php
+
+class GlobalController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function okAction()
+    {
+        // action body
+    }
+
+    public function errAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
