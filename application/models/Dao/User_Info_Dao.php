fb7ff0c6274495b73bb2d949a742ddb6af7d4224
Zhengwei Li <lzw900420@gmail.com>
2015-04-25 16:40:54 -0400

    user_info's CRUD

--- application/models/Dao/User_Info_Dao.php
+++ application/models/Dao/User_Info_Dao.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ *
+ * @author Zhengwei
+ *
+ */
+
 class studyingIn_Model_User_Info_Dao extends Zend_Db_Table_Abstract {
 
 	protected $_name = "studyingIn_user_info";
@@ -21,6 +27,89 @@ class studyingIn_Model_User_Info_Dao extends Zend_Db_Table_Abstract {
 			}
 
 			$row->save();
+			return true;
+
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * get user's info from database
+	 *
+	 * @param int/array
+	 * @return array/null
+	 */
+	public function get_user_info($where) {
+
+		if (is_numeric($where)) {
+			$row = $this->find($where)->current();
+		}
+
+		if (is_array($where)) {
+			$select = $this->select();
+			if (count($where) > 0) {
+				foreach ($where as $key => $value) {
+					$select->where($key . '=?', $value);
+				}
+			}
+			$row = $this->fetchAll($select);
+		}
+
+		if ($row) {
+			return $row;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * update user's info
+	 *
+	 * @param 1: array new data
+	 * @param 2: array where(id/uuid)
+	 * @return bool
+	 */
+	public function update_user_info($newdata, $where) {
+
+		$where_cluster;
+		if (is_numeric($where)) {
+			$where_cluster = $this->getAdapter()->quoteInto('user_id =?', $where);
+			//print("11");
+		}
+		if (is_string($where)) {
+			$where_cluster = $this->getAdapter()->quoteInto('user_uuid =?', $where);
+		}
+
+		$row = $this->update($newdata, $where_cluster);
+	}
+
+	/**
+	 * delete user
+	 *
+	 * @param int/string/array
+	 * @return bool
+	 */
+	public function delete_user_info($where) {
+
+		$where_cluster;
+		if (is_numeric($where)) {
+			$where_cluster = $this->getAdapter()->quoteInto('user_id =?', $where);
+		}
+		if (is_string($where)) {
+			$where_cluster = $this->getAdapter()->quoteInto('user_uuid =?', $where);
+		}
+
+		if (is_array($where)) {
+			if (count($where) > 0) {
+				foreach ($where as $key => $value) {
+					$where_cluster[$key . '=?'] = $value;
+				}
+			}
+		}
+
+		$row = $this->delete($where_cluster);
+		if ($row) {
 			return true;
 		} else {
 			return false;
