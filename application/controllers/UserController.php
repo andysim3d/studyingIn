c427dce06d95742c74a30f975a88579a6595586a
Zhengwei Li <lzw900420@gmail.com>
2015-04-30 17:39:17 -0400

    register,active,login,resetpassword,changepassword,logout

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,10 +1,18 @@
 <?php
 
 header("Content-type: text/html;charset=utf-8");
+require_once APPLICATION_PATH . '/controllers/BaseController.php';
 require_once APPLICATION_PATH . '/utils/UUID.php';
 require_once APPLICATION_PATH . '/utils/Email_Sender.php';
+require_once APPLICATION_PATH . '/utils/Tools.php';
 require_once APPLICATION_PATH . '/models/Dao/User_Dao.php';
 require_once APPLICATION_PATH . '/models/Dao/User_Info_Dao.php';
+require_once APPLICATION_PATH . '/forms/User_Register_Form.php';
+require_once APPLICATION_PATH . '/forms/User_Login_Form.php';
+require_once APPLICATION_PATH . '/forms/User_Find_Password_Form.php';
+require_once APPLICATION_PATH . '/forms/User_Reset_Password_Form.php';
+require_once APPLICATION_PATH . '/forms/User_Change_Password_Form.php';
+
 class UserController extends Zend_Controller_Action {
 
 	public function init() {
@@ -13,6 +21,7 @@ class UserController extends Zend_Controller_Action {
 
 	public function indexAction() {
 		// action body
+
 	}
 
 	/**
@@ -20,101 +29,342 @@ class UserController extends Zend_Controller_Action {
 	 *
 	 */
 	public function registerAction() {
-		$formRegister = new studyingIn_Form_User();
-		$formRegister->removeElement('salt');
-		$formRegister->removeElement('user_uuid');
-		$formRegister->removeElement('user_actived');
-		//$formRegister->removeElement('user_remember');
-		$this->view->formRegister = $formRegister;
+
+		$register_form = new StudyingIn_User_Register_Form();
 
 		if ($this->getRequest()->isPost()) {
 
-			if ($formRegister->isValid($_POST)) {
-				$userData = $formRegister->getValues();
+			if ($register_form->isValid($_POST)) {
+				$user_data = $register_form->getValues();
 			} else {
 				$this->view->info = 1;
 				$this->_forward('err', 'global');
 			}
 
-			$userData['user_uuid'] = 'user-' . UUID::v4();
-			$userDao = new studyingIn_Model_User_Dao();
-			$newUser_id = $userDao->create_user($userData);
+			$user_dao = new StudyingIn_Model_User_Dao();
+			// $user_email_exist = $user_dao->check_user_existence_by_user_email($user_data['user_email']);
+			// $user_name_exist = $user_dao->check_user_existence_by_user_name($user_data['user_name']);
 
-			$userInfo = $userData;
-			unset($userInfo['salt']);
-			unset($userInfo['user_password']);
-			unset($userInfo['confirm_password']);
-			unset($userInfo['user_actived']);
-			$userInfo['user_id'] = $newUser_id;
-			$userInfo['user_introduction'] = "该用户很懒，暂时什么都没有留下。";
-			$userInfo['user_create_time'] = date("Y-m-d H:i:s", time());
+			if (!$user_name_exist && !$user_email_exist) {
+				$new_user = $user_dao->create_new_user($user_data);
 
-			$userInfoDao = new studyingIn_Model_User_Info_Dao();
-			$newUserInfo = $userInfoDao->create_user_info($userInfo);
+				//create the user_info, and save to studyingIn_user_info
+				foreach ($new_user as $key => $value) {
+					$user_info[$key] = $value;
+				}
 
-			if ($newUser_id && $newUserInfo) {
-				$sendActiveEmail = Email_Sender::sendActivationEmail($userData['user_name'], $userData['user_email'], $userData['salt']);
-				if ($sendActiveEmail) {
-					//TODO: jump to the index/index, and alert('we have send you a confirm email, please active your account with the url in the email ASAP.')
+				$user_info_dao = new StudyingIn_Model_User_Info_Dao();
+				$new_user_info = $user_info_dao->create_new_user_info($user_info);
+
+				if (count($new_user) != 0 && $new_user_info) {
+
+					$send_active_email = Email_Sender::sendActivationEmail($new_user['user_name'], $new_user['user_email'], $new_user['salt']);
 					$this->view->info = 1;
 					$this->_forward('ok', 'global');
 				}
+			} else {
+				$this->view->info = 10;
+				$this->_forward('err', 'global');
 			}
-
 		}
 
 	}
 
+	/**
+	 *
+	 * active
+	 *
+	 */
+
 	public function activeAction() {
-		// action body
+
 		$rcode = base64_decode($this->getRequest()->getParam('l', ''));
 		list($email, $code, $time) = explode('#', $rcode);
 
-		$userDao = new studyingIn_Model_User_Dao();
-		$where = array('user_email' => $email);
-		$user = $userDao->get_user($where);
+		$user_dao = new StudyingIn_Model_User_Dao();
+		$user = $user_dao->get_user_by_user_email($email);
 
-		if (!$user || $code != hash('sha1', $email . $user['salt'])) {
-			throw new Exception("Incorrect validation code!");
-		} else {
-			if (time() > ($time + 60 * 60 * 24)) {
-				//TODO:the email is over time...
+		if (count($user) != 0) {
+			if ($user[0]['user_actived'] == 0) {
+				if ($code != hash('sha1', $user[0]['user_email'] . $user[0]['salt'])) {
+					//throw new Exception("Incorrect validation code!");
+					$this->view->info = 8;
+					$this->_forward("err", "global");
+				} else {
+					if (time() > ((int) $time + 60 * 60 * 24)) {
+						$this->view->info = 6;
+						$this->view->id = $user[0]['user_id'];
+						$this->view->uuid = $user[0]['user_uuid'];
+						$this->_forward("err", "global");
+					} else {
+						$ok = $user_dao->active_user_by_user_id($user[0]['user_id']);
+						if ($ok) {
+							$sess = new Zend_Session_Namespace();
+							$sess->user_id = $user[0]['user_id'];
+							$this->_redirect('/index/index');
+						}
+					}
+				}
 			} else {
-				$userDap->active_user_by_user_id($user['user_id']);
-				session_start();
-				$_SESSION['user_id'] = $user['user_id'];
-				$this->_redirect('user/account/id/' . $id);
-				//TODO:jump to the home page
+				$this->view->info = 7;
+				$this->_forward("err", "global");
 			}
+		} else {
+			$this->view->info = 8;
+			$this->_forward("err", "global");
 		}
 	}
+
+	/**
+	 *
+	 * login
+	 *
+	 */
 
 	public function loginAction() {
 
-		$formRegister = new studyingIn_Form_User();
-		$formRegister->removeElement('user_id');
-		$formRegister->removeElement('user_name');
-		$formRegister->removeElement('user_school');
-		$formRegister->removeElement('salt');
-		$formRegister->removeElement('user_uuid');
-		$formRegister->removeElement('user_actived');
-		$formRegister->removeElement('confirm_password');
-		$this->view->formRegister = $formRegister;
+		$login_form = new StudyingIn_User_Login_Form();
 
 		if ($this->getRequest()->isPost()) {
 
-			if ($formRegister->isValid($_POST)) {
-				$userData = $formRegister->getValues();
-			}
+			if ($login_form->isValid($_POST)) {
+				$user_data = $login_form->getValues();
 
-			$userDao = new studyingIn_Model_User_Dao();
-			$user = $userDao->get_user(array('user_email' => $userData['user_email']));
+				// if ($user_data['user_remember'] == 0) {
+				// 	if (isset($_COOKIE['email'])) {
+				// 		setcookie('email', $user_email, time() - 100, '/', 'studyingin.com', false, true);
+				// 	}
+				// } else {
+				// 	setcookie('email', $user_email, time() + 2 * 7 * 24 * 3600, '/', 'studyingin.com', false, true);
+				// }
+
+				$user_dao = new StudyingIn_Model_User_Dao();
+				$user = $user_dao->get_user_by_user_email($user_data['user_email']);
+
+				if (count($user) != 0) {
+					$salted_password = hash('sha256', $user_data['user_password'] . $user[0]['salt']);
+					if ($salted_password != $user[0]['user_password']) {
+						//密码错误
+						$this->view->info = 4;
+						$this->_forward('err', 'global');
+					} else if ($user[0]['user_actived'] == 0) {
+						//尚未激活
+						$this->view->info = 5;
+						$this->view->id = $user[0]['user_id'];
+						$this->view->uuid = $user[0]['user_uuid'];
+						$this->_forward('err', 'global');
+					} else {
+						//登陆成功
+						$sess = new Zend_Session_Namespace();
+						$sess->user_id = $user[0]['user_id'];
+						$this->view->info = 2;
+						$this->_forward('ok', 'global');
+					}
+				} else {
+					//账号错误或尚未注册
+					$this->view->info = 3;
+					$this->_forward('err', 'global');
+				}
+			} else {
+				//输入格式错误
+				$this->view->info = 2;
+				$this->_forward('err', 'global');
+			}
 		}
 
 	}
 
+	/**
+	 * resend confirm email
+	 *
+	 */
+
+	public function resendactiveemailAction() {
+
+		$id = $this->getRequest()->getParam('uid', '');
+		$uuid = $this->getRequest()->getParam('id', '');
+		$user_dao = new StudyingIn_Model_User_Dao();
+		$user = $user_dao->get_user_by_user_id($id);
+		if (count($user) != 0) {
+			if ($uuid != $user[0]['user_uuid']) {
+				$this->view->info = 9;
+				$this->_forward('err', 'global');
+			} else {
+				$send_active_email = Email_Sender::sendActivationEmail($user[0]['user_name'], $user[0]['user_email'], $user[0]['salt']);
+				$this->view->info = 1;
+				$this->_forward('ok', 'global');
+			}
+		} else {
+			$this->view->info = 9;
+			$this->_forward('err', 'global');
+		}
+	}
+
+	/**
+	 * logout
+	 *
+	 */
 	public function logoutAction() {
 		// action body
+		Zend_Session::destroy();
+
+	}
+
+	/**
+	 *
+	 * find password
+	 *
+	 */
+	public function findpasswordAction() {
+
+		$find_password_form = new StudyingIn_User_Find_Password_Form();
+		if ($this->getRequest()->isPost()) {
+			if ($find_password_form->isValid($_POST)) {
+				$user_data = $find_password_form->getValues();
+				$user_dao = new StudyingIn_Model_User_Dao();
+				$user = $user_dao->get_user_by_user_email($user_data['user_email']);
+				if (count($user) != 0) {
+					$send_reset_password_email = Email_Sender::sendResetPasswordEmail($user[0]['user_name'], $user_data['user_email'], $user[0]['user_password']);
+					$this->view->info = 4;
+					$this->_forward('ok', 'global');
+				} else {
+					echo "string";
+				}
+			} else {
+				$this->view->info = 11;
+				$this->_forward('err', 'global');
+			}
+
+		}
+	}
+
+	/**
+	 *
+	 * reset password ui
+	 *
+	 */
+	public function resetpassworduiAction() {
+
+		$rcode = base64_decode($this->getRequest()->getParam('r', ''));
+		list($email, $password, $time) = explode('#', $rcode);
+		// echo $email . '<br>' . $password . '<br>' . $time;
+		// exit;
+
+		if (time() > (int) $time + 24 * 60 * 60) {
+			$this->view->info = 12;
+			$this->_forward('err', 'global');
+		}
+		$user_dao = new StudyingIn_Model_User_Dao();
+		$user = $user_dao->get_user_by_user_email($email);
+		if (count($user) != 0) {
+			if ($password != $user[0]['user_password']) {
+				$this->view->info = 13;
+				$this->_forward('err', 'global');
+			}
+			$this->view->id = $user[0]['user_uuid'];
+			$this->view->email = $email;
+		} else {
+			$this->view->info = 9;
+			$this->_forward('err', 'global');
+		}
+
+	}
+
+	/**
+	 *
+	 * reset password
+	 *
+	 */
+	public function resetpasswordAction() {
+
+		$uuid = $this->getRequest()->getParam('r', '');
+
+		$rest_password_form = new StudyingIn_User_Reset_Password_Form();
+		if ($this->getRequest()->isPost()) {
+			if ($rest_password_form->isValid($_POST)) {
+				$user_data = $rest_password_form->getValues();
+			} else {
+				$this->view->info = 14;
+				$this->_forward('err', 'global');
+			}
+			$user_dao = new StudyingIn_Model_User_Dao();
+			$user = $user_dao->get_user_by_user_email($user_data['user_email']);
+
+			if (count($user) != 0) {
+				if ($user[0]['user_uuid'] == $uuid) {
+					$ok = $user_dao->update_password_by_user_email($user_data['user_password'], $user[0]['salt'], $user_data['user_email']);
+					if ($ok) {
+						$this->view->info = 5;
+						$this->_forward('ok', 'global');
+					} else {
+						$this->view->info = 15;
+						$this->_forward('err', 'global');
+
+					}
+				} else {
+					$this->view->info = 15;
+					$this->_forward('err', 'global');
+				}
+
+			} else {
+				$this->view->info = 15;
+				$this->_forward('err', 'global');
+			}
+		}
+	}
+
+	/**
+	 *
+	 * change password ui
+	 *
+	 */
+	public function changepassworduiAction() {
+
+	}
+
+	/**
+	 *
+	 * change password
+	 *
+	 */
+	public function changepasswordAction() {
+
+		$change_password_form = new StudyingIn_User_Change_Password_Form();
+
+		if ($this->getRequest()->isPost()) {
+			if ($change_password_form->isValid($_POST)) {
+				$data = $change_password_form->getValues();
+			} else {
+				$this->view->info = 16;
+				$this->_forward('err', 'global');
+			}
+		}
+		$sess = new Zend_Session_Namespace();
+		$user_id = $sess->user_id;
+
+		$user_dao = new StudyingIn_Model_User_Dao();
+		$user = $user_dao->get_user_by_user_id($user_id);
+
+		if (count($user) != 0) {
+			if ($user_dao->authenticate_user_by_password($data['old_password'], $user[0]['salt'], $user[0]['user_password'])) {
+				$ok = $user_dao->update_password_by_user_email($data['user_password'], $user[0]['salt'], $user[0]['user_email']);
+				if ($ok) {
+
+					$this->view->info = 6;
+					$this->_forward('ok', 'global');
+				} else {
+					$this->view->info = 17;
+					$this->_forward('err', 'global');
+				}
+			} else {
+				$this->view->info = 17;
+				$this->_forward('err', 'global');
+			}
+		} else {
+			$this->view->info = 17;
+			$this->_forward('err', 'global');
+		}
 	}
 
 }
+?>
