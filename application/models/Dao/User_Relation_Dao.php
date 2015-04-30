06b075350ded7f0ffebeabe9453127f27d1e2368
Zhengwei Li <lzw900420@gmail.com>
2015-04-29 23:32:59 -0400

    optimise User_relation's CRUD

--- application/models/Dao/User_Relation_Dao.php
+++ application/models/Dao/User_Relation_Dao.php
@@ -10,108 +10,168 @@ class StudyingIn_Model_User_Relation_Dao extends Zend_Db_Table_Abstract {
 
 	protected $_name = "studyingIn_user_relation";
 
+	//C
+	private function create_relation($user_relation) {
+
+		$row = $this->createRow();
+		foreach ($user_relation as $key => $value) {
+			$row[$key] = $value;
+		}
+		$row['follow_since'] = date("Y-m-d H:i:s", time());
+
+		try {
+			$row->save();
+		} catch (Exception $e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	//R
+	private function get_relation($where) {
+
+		try {
+			$row = $this->fetchAll($where);
+		} catch (Exception $e) {
+			return false;
+		}
+		return $row;
+	}
+
+	//D
+	private function delete_relation($where) {
+
+		try {
+			$row = $this->delete($where);
+		} catch (Exception $e) {
+			return false;
+		}
+
+		return true;
+	}
+
 	/**
-	 * create a relation
+	 * create a new relation
 	 *
 	 * @param array
 	 * @return bool
 	 */
-	public function create_user_relation($user_relation) {
+	public function create_new_relation($following_id, $follower_id) {
 
-		$row = $this->createRow();
-
-		if (count($user_relation) > 0) {
-
-			foreach ($user_relation as $key => $value) {
-				$row->$key = $value;
-			}
-
-			$row->save();
-			return true;
-
+		if ($following_id != $follower_id) {
+			$user_relation = array('following_id' => $following_id,
+				'follower_id' => $follower_id);
+			return $this->create_relation($user_relation);
 		} else {
 			return false;
 		}
+
 	}
 
 	/**
-	 * get user's relation
+	 * get count of the user's following
 	 *
-	 * @param array
-	 * @return $row/null
+	 * @param int: follower's id
+	 * @return int
 	 */
-	private function get_user_relation($where) {
-		//echo "string";
-		//print_r($where);
-		$select = $this->select();
-		if (count($where) > 0) {
-			foreach ($where as $key => $value) {
-				$select->where($key . '=?', $value);
-			}
-		}
-		$row = $this->fetchAll($select);
+	public function get_count_of_user_following($follower_id) {
 
-		if ($row) {
-			return $row;
-		} else {
-			return null;
-		}
+		$where = $this->getAdapter()->quoteInto('follower_id =?', $follower_id);
+
+		return count($this->get_relation($where));
+	}
+
+	/**
+	 * get count of the user's follower
+	 *
+	 * @param int: following's id
+	 * @return int
+	 */
+	public function get_count_of_user_follower($following_id) {
+
+		$where = $this->getAdapter()->quoteInto('following_id =?', $following_id);
+
+		return count($this->get_relation($where));
 	}
 
 	/**
 	 * get user's following
 	 *
-	 * @param follower's id
+	 * @param int:follower's id
 	 * @return $row/null
 	 */
 	public function get_user_following($follower_id) {
 
-		$where = array('follower_id' => $follower_id);
+		$where = $this->getAdapter()->quoteInto('follower_id =?', $follower_id);
 
-		return $this->get_user_relation($where);
+		return $this->get_relation($where);
 	}
 
 	/**
 	 * get user's follower
 	 *
-	 * @param follower's id
+	 * @param int:follower's id
 	 * @return $row/null
 	 */
 	public function get_user_follower($following_id) {
 
-		$where = array('following_id' => $following_id);
+		$where = $this->getAdapter()->quoteInto('following_id =?', $following_id);
 
-		return $this->get_user_relation($where);
+		return $this->get_relation($where);
 	}
 
 	/**
-	 * delete user's album
+	 * check if following
 	 *
-	 * @param relation_id/array('following_id'=>1,'folower_id'=>2);
+	 * @param int 1: following_id
+	 * @param int 2: follower_id
 	 * @return bool
 	 */
-	public function delete_relation($where) {
+	public function check_if_following($following_id, $follower_id) {
 
-		$where_cluster;
-		if (is_numeric($where)) {
-			$where_cluster = $this->getAdapter()->quoteInto('relation_id =?', $where);
-		}
-
-		if (is_array($where)) {
-			if (count($where) > 0) {
-				foreach ($where as $key => $value) {
-					$where_cluster[$key . '=?'] = $value;
-				}
-			}
-		}
-
-		$row = $this->delete($where_cluster);
-		if ($row) {
+		$where = $this->getAdapter()->quoteInto('following_id =?', $following_id) .
+		$this->getAdapter()->quoteInto(' AND follower_id =?', $follower_id);
+		$res = $this->get_relation($where);
+		if (count($res) != 0) {
 			return true;
 		} else {
 			return false;
 		}
+	}
 
+	/**
+	 * delete user's by relation_id
+	 *
+	 * @param relation_id
+	 * @return bool
+	 */
+	public function delete_relation_by_id($relation_id) {
+
+		$where = $this->getAdapter()->quoteInto('relation_id =?', $relation_id);
+		return $this->delete_relation($where);
+
+	}
+
+	/**
+	 * delete user's by following_id & follower_id
+	 *
+	 * @param int 1: following_id
+	 * @param int 2: follower_id
+	 * @return bool
+	 */
+	public function delete_relation_by_both_id($following_id, $follower_id) {
+
+		$res = $this->check_if_following($following_id, $follower_id);
+		if ($res) {
+			$where = $this->getAdapter()->quoteInto('following_id =?', $following_id) .
+			$this->getAdapter()->quoteInto(' AND follower_id =?', $follower_id);
+
+			return $this->delete_relation($where);
+		} else {
+			return true;
+
+		}
 	}
 
 }
